4598859a1606f338a24d5989297d69c402919458
Shady Zahran <zahran.shady@gmail.com>
2022-06-29 21:53:56 +0200

    - Build on Gamemanager logic to control UI - Added modular structure for landmark data for ease of modification

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Gamemanager : MonoBehaviour
+{
+
+    public GameObject Menu_MainMenu;
+
+    public Camera camera_Map, camera_Depth;
+
+    public GameObject Menu_Landmark;
+    public TMP_Text text_LandmarkTitle;
+    public Image image_LandmarkImage;
+
+
+    public Landmark[] Landmarks;
+
+    public void StartGame()
+    {
+        camera_Map.enabled = true;
+        camera_Depth.enabled = true;
+
+        Menu_MainMenu.SetActive(false);
+    }
+
+    public void Btn_OpenLandmarkMenu(string name_Landmark)
+    {
+        for (int i = 0; i < Landmarks.Length; i++)
+        {
+            if (Landmarks[i].name.Equals(name_Landmark))
+            {
+                //fill landmark title and image
+                FillLandmarkInfo(Landmarks[i].data);
+                break;
+            }
+        }
+        
+    }
+    //exit landmark and go back to map view
+    public void Btn_CloseLandmarkMenu()
+    {
+
+    }
+    //open interaction menu
+    public void Btn_OpenLandmarkInteractionMenu()
+    {
+
+    }
+    //go to next dialogue/image entry for the landmark
+    public void Btn_LandmarkInteraction_Next()
+    {
+
+    }
+    //go to previous dialogue/image entry for the landmark
+    public void Btn_LandmarkInteraction_Previous()
+    {
+
+    }
+
+    void FillLandmarkInfo(LandmarkData _data)
+    {
+        Debug.Log("The location name from entry is: " + _data.landmarkName);
+    }
+
+    [System.Serializable]
+    public struct Landmark
+    {
+        public string name;
+        public LandmarkData data;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Btn_OpenLandmarkMenu("Dom");
+        }
+    }
+}
---
+++ Assets/Scripts/LandmarkData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Landmark_Data", menuName = "LandmarkData/Data", order = 1)]
+public class LandmarkData : ScriptableObject
+{
+    public string landmarkName;
+    public List<DialogueEntry> text_dialogues;
+
+    //public List<Image> image_dialogues;
+
+    [System.Serializable]
+    public struct DialogueEntry
+    {
+        public string text;
+        public Image dialogueImage;
+    }
+
+}
