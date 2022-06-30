a50776e37dfb2172a81eaf7bc5b2f5add8334367
JannoBoy <Jan.guenther.97@web.de>
2022-06-30 11:59:32 +0200

    Firebase setup

---
+++ Assets/Scripts/PointOfInterestManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PointOfInterestManagerScript : MonoBehaviour
+{
+    private MeshRenderer myMesh;
+
+    [SerializeField]
+    private Material playerEnteredMaterial;
+
+    private Material myBasicMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMesh = GetComponent<MeshRenderer>();
+
+        myBasicMaterial = myMesh.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            myMesh.material = playerEnteredMaterial;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            myMesh.material = myBasicMaterial;
+        }
+    }
+}
